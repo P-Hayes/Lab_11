44e8dd73d317d345206c694bcf98c0b1b27cc132
Patrick Hayes <pahayes92@gmail.com>
2016-04-28 18:06:30 +0100

    completed lab 11 sheet

--- app/src/main/java/com/devboy/patrick/lab11/MainActivity.java
+++ app/src/main/java/com/devboy/patrick/lab11/MainActivity.java
@@ -1,7 +1,11 @@
 package com.devboy.patrick.lab11;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btnMain = (Button)findViewById(R.id.btn_click);
+        btnMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Context context = getApplicationContext();
+                CharSequence text = getString(R.string.btn_clicked);
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        });
     }
 }
